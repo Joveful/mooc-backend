384b2d3e839e2347066f6803758fea9778193ff8
jvehk@LAPTOP-J81G0J0F <j.vehkakoski@gmail.com>
2024-08-23 12:58:18 +0300

    osa3 morgan

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 
 let persons = [
   {
@@ -23,11 +25,67 @@ let persons = [
   }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain'})
-  response.end(JSON.stringify(persons))
+app.use(express.json())
+
+morgan.token('post', (request, response) => {return JSON.stringify(request.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body
+  const duplicate = persons.find(
+    person => person.name === body.name
+  )
+
+  if (duplicate) {
+    return response.status(400).json({
+      error: "Person is already in phonebook"
+    })
+  }
+
+  if (!(body.name && body.number)) {
+    return response.status(400).json({
+      error: "Name or number missing"
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date(Date.now())
+  response.send(`Phonebook has info of ${persons.length} persons\n\n${date}`)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
