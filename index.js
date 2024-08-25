8967d4ea8a558c24e36b40a9c2a04742ad637d67
jvehk@LAPTOP-J81G0J0F <j.vehkakoski@gmail.com>
2024-08-25 06:41:33 +0300

    osa3.21 deploy

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -36,7 +38,7 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
   const body = request.body
   
   if (!(body.name && body.number)) {
@@ -53,6 +55,7 @@ app.post('/api/persons/', (request, response) => {
   person.save().then(newPerson => {
     response.json(newPerson)
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -76,14 +79,13 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, 
+    {new: true, runValidators: true, context: 'query'}
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
--- models/person.js
+++ models/person.js
@@ -14,8 +14,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+          return /\d{2,3}-\d{4}/.test(v)
+      }
+    },
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
